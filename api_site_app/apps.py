38ffeafeb36bd4c0406f1e3b6d444c455a0b3bec
MAMADOU LAMARANA DIALLO <lamarana55@gmail.com>
2022-07-22 18:00:10 +0000

    Add App site

---
+++ api_site_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiSiteAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_site_app'
--- rest_api/settings.py
+++ rest_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_swagger',
     'api.apps.ApiConfig',
+    'api_site_app.ApiConfig',
 ]
 
 MIDDLEWARE = [
